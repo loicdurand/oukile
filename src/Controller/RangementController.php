4670b54475fca6582371d9cca480ac3f2545c85f
Loïc DURAND <loicdurand.ld@gmail.com>
2026-01-20 20:34:42 +0000

    Création d'un type de rangement directement dans le formulaire de création de renagement

--- src/Controller/RangementController.php
+++ src/Controller/RangementController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Rangement;
+use App\Entity\TypeRangement;
 use App\Form\RangementType;
 use App\Repository\RangementRepository;
+use App\Repository\TypeRangementRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ZoneRepository;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use App\Entity\User;
 
 #[Route('/rangement')]
 final class RangementController extends AbstractController
@@ -23,20 +28,57 @@ final class RangementController extends AbstractController
     }
 
     #[Route('/new', name: 'oukile_rangement_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(#[CurrentUser] ?User $user, Request $request, EntityManagerInterface $entityManager, ZoneRepository $zoneRepository, TypeRangementRepository $typeRangementRepository): Response
     {
+
+        if (is_null($user))
+            return $this->redirectToRoute('oukile_login');
+
         $rangement = new Rangement();
+        $zoneId = $request->query->get('id');
+        if ($zoneId) {
+            $zone = $zoneRepository->find($zoneId);
+            if ($zone) {
+                $rangement->setZone($zone);
+            }
+        }
         $form = $this->createForm(RangementType::class, $rangement);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($rangement);
-            $entityManager->flush();
+        if ($form->isSubmitted()) {
+            // Vérifier si un nouveau type a été créé
+            $createNewType = $form->get('createNewType')->getData();
+            if ($createNewType) {
+                $newTypeName = $form->get('newTypeName')->getData();
+                if ($newTypeName) {
+                    $newType = new TypeRangement();
+                    $newType->setNom($newTypeName);
+                    $entityManager->persist($newType);
+                    $entityManager->flush();
+                    $rangement->setType($newType);
+                }
+            }
 
-            return $this->redirectToRoute('oukile_rangement_index', [], Response::HTTP_SEE_OTHER);
+            // Valider que si on ne crée pas un nouveau type, le type doit être sélectionné
+            if (!$createNewType && !$rangement->getType()) {
+                // Le formulaire n'est pas valid, on réaffiche
+                return $this->render('rangement/new.html.twig', [
+                    'user' => $user,
+                    'rangement' => $rangement,
+                    'form' => $form,
+                ]);
+            }
+
+            if ($form->isValid()) {
+                $entityManager->persist($rangement);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('oukile_rangement_index', [], Response::HTTP_SEE_OTHER);
+            }
         }
 
         return $this->render('rangement/new.html.twig', [
+            'user' => $user,
             'rangement' => $rangement,
             'form' => $form,
         ]);
@@ -51,15 +93,39 @@ final class RangementController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'oukile_rangement_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Rangement $rangement, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Rangement $rangement, EntityManagerInterface $entityManager, TypeRangementRepository $typeRangementRepository): Response
     {
         $form = $this->createForm(RangementType::class, $rangement);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+        if ($form->isSubmitted()) {
+            // Vérifier si un nouveau type a été créé
+            $createNewType = $form->get('createNewType')->getData();
+            if ($createNewType) {
+                $newTypeName = $form->get('newTypeName')->getData();
+                if ($newTypeName) {
+                    $newType = new TypeRangement();
+                    $newType->setNom($newTypeName);
+                    $entityManager->persist($newType);
+                    $entityManager->flush();
+                    $rangement->setType($newType);
+                }
+            }
 
-            return $this->redirectToRoute('oukile_rangement_index', [], Response::HTTP_SEE_OTHER);
+            // Valider que si on ne crée pas un nouveau type, le type doit être sélectionné
+            if (!$createNewType && !$rangement->getType()) {
+                // Le formulaire n'est pas valid, on réaffiche
+                return $this->render('rangement/edit.html.twig', [
+                    'rangement' => $rangement,
+                    'form' => $form,
+                ]);
+            }
+
+            if ($form->isValid()) {
+                $entityManager->flush();
+
+                return $this->redirectToRoute('oukile_rangement_index', [], Response::HTTP_SEE_OTHER);
+            }
         }
 
         return $this->render('rangement/edit.html.twig', [
--- src/Form/RangementType.php
+++ src/Form/RangementType.php
@@ -7,24 +7,57 @@ use App\Entity\TypeRangement;
 use App\Entity\Zone;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RangementType extends AbstractType
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nom')
-            ->add('zone', EntityType::class, [
-                'class' => Zone::class,
-                'choice_label' => 'id',
-            ])
             ->add('type', EntityType::class, [
                 'class' => TypeRangement::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nom',
+                'placeholder' => 'Sélectionner un type',
+                'required' => false,
+            ])
+            ->add('zone', EntityType::class, [
+                'class' => Zone::class,
+                'choice_label' => 'nom',
             ])
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            $data = $event->getData();
+
+            // Ajouter l'option de créer un nouveau type
+            $form->add('createNewType', CheckboxType::class, [
+                'mapped' => false,
+                'required' => false,
+                'label' => 'Créer un nouveau type de rangement',
+            ]);
+
+            // Ajouter le champ pour saisir le nom du nouveau type
+            $form->add('newTypeName', TextType::class, [
+                'mapped' => false,
+                'required' => false,
+                'label' => 'Nom du nouveau type',
+            ]);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
