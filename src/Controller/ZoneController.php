5b4257d39dfa3dae613ee4455b605a6dd5d9477e
Loïc DURAND <loicdurand.ld@gmail.com>
2026-03-09 10:06:17 -0400

    Entité Zone traitée

--- src/Controller/ZoneController.php
+++ src/Controller/ZoneController.php
@@ -12,31 +12,34 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/zone')]
+#[Route("/zone")]
 final class ZoneController extends AbstractController
 {
-    #[Route(name: 'oukile_zone_index', methods: ['GET'])]
+    #[Route(name: "oukile_zone_index", methods: ["GET"])]
     public function index(ZoneRepository $zoneRepository): Response
     {
-        return $this->render('zone/index.html.twig', [
-            'zones' => $zoneRepository->findAll(),
+        return $this->render("zone/index.html.twig", [
+            "zones" => $zoneRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'oukile_zone_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, PieceRepository $pieceRepository): Response
-    {
+    #[Route("/new", name: "oukile_zone_new", methods: ["GET", "POST"])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        PieceRepository $pieceRepository,
+    ): Response {
         $zone = new Zone();
-        
+
         // Récupérer le paramètre 'id' de l'URL pour pré-sélectionner la pièce
-        $pieceId = $request->query->get('id');
+        $pieceId = $request->query->get("id");
         if ($pieceId) {
             $piece = $pieceRepository->find($pieceId);
             if ($piece) {
                 $zone->setPiece($piece);
             }
         }
-        
+
         $form = $this->createForm(ZoneType::class, $zone);
         $form->handleRequest($request);
 
@@ -44,49 +47,72 @@ final class ZoneController extends AbstractController
             $entityManager->persist($zone);
             $entityManager->flush();
 
-            return $this->redirectToRoute('oukile_unite_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute(
+                "oukile_unite_show",
+                ["id" => $zone->getPiece()->getUnite()->getId()],
+                Response::HTTP_SEE_OTHER,
+            );
         }
 
-        return $this->render('zone/new.html.twig', [
-            'zone' => $zone,
-            'form' => $form,
+        return $this->render("zone/new.html.twig", [
+            "zone" => $zone,
+            "form" => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'oukile_zone_show', methods: ['GET'])]
+    #[Route("/{id}", name: "oukile_zone_show", methods: ["GET"])]
     public function show(Zone $zone): Response
     {
-        return $this->render('zone/show.html.twig', [
-            'zone' => $zone,
+        return $this->render("zone/show.html.twig", [
+            "zone" => $zone,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'oukile_zone_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Zone $zone, EntityManagerInterface $entityManager): Response
-    {
+    #[Route("/{id}/edit", name: "oukile_zone_edit", methods: ["GET", "POST"])]
+    public function edit(
+        Request $request,
+        Zone $zone,
+        EntityManagerInterface $entityManager,
+    ): Response {
         $form = $this->createForm(ZoneType::class, $zone);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('oukile_unite_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute(
+                "oukile_unite_index",
+                [],
+                Response::HTTP_SEE_OTHER,
+            );
         }
 
-        return $this->render('zone/edit.html.twig', [
-            'zone' => $zone,
-            'form' => $form,
+        return $this->render("zone/edit.html.twig", [
+            "zone" => $zone,
+            "form" => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'oukile_zone_delete', methods: ['POST'])]
-    public function delete(Request $request, Zone $zone, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $zone->getId(), $request->getPayload()->getString('_token'))) {
+    #[Route("/{id}", name: "oukile_zone_delete", methods: ["POST"])]
+    public function delete(
+        Request $request,
+        Zone $zone,
+        EntityManagerInterface $entityManager,
+    ): Response {
+        if (
+            $this->isCsrfTokenValid(
+                "delete" . $zone->getId(),
+                $request->getPayload()->getString("_token"),
+            )
+        ) {
             $entityManager->remove($zone);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('oukile_unite_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute(
+            "oukile_unite_show",
+            ["id" => $zone->getPiece()->getUnite()->getId()],
+            Response::HTTP_SEE_OTHER,
+        );
     }
 }
