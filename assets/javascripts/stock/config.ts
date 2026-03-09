87ada7498c66693caed7a8e4596dc40fd087ebe0
Loïc DURAND <loicdurand.ld@gmail.com>
2026-03-09 11:06:11 -0400

    Prise en compte de l'ID de l"unité après connexion de l'utilisateur pour toute la partie JS/TS

--- assets/javascripts/stock/config.ts
+++ assets/javascripts/stock/config.ts
@@ -1,3 +1,9 @@
-export const UNITE_ID = 1;
-export const API_BASE = '/oukile/api';
+const login = document.getElementById("login");
+let unite_id = 0;
+if (login !== null) {
+    const { dataset } = login;
+    unite_id = +(dataset.unite || 0);
+}
+export const UNITE_ID = unite_id;
+export const API_BASE = "/oukile/api";
 export const SEARCH_DEBOUNCE_MS = 300;
